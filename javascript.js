618a6c1782387877c988216f0a9f3d3d14569903
Shoaib-malik-420 <101936344+Shoaib-malik-420@users.noreply.github.com>
2022-04-19 17:51:55 +0500

    Add files via upload
    marksheet

---
+++ javascript.js
@@ -0,0 +1,60 @@
+// var r = parseInt( prompt("enter any number"));
+// var h = parseInt( prompt("enter any number"));
+
+// var result = (3.14 * r ) * (r + ((h * h) + (r * r)) );
+// document.write("<h1> result of cone is :" + result + "</h1>");
+
+
+// var y = parseInt( prompt("enter any number"));
+// var u = parseInt( prompt("enter any number"));
+
+// var answer = ((2 * 3.14 * r * h) + (2 * 3.14 * r * r));
+// document.write("<h1> answer area is :" + answer + "</h1>");
+
+
+// var v = parseInt( prompt("enter any number"));
+// var m = parseInt( prompt("enter any number"));
+
+// var vis = (3.14 * r *r *h);
+// document.write("<h1> answer of velocity is :" + vis + "</h1>");
+
+// var a = parseFloat( prompt("enter any number"));
+// var b = parseFloat( prompt("enter any number"));
+
+// var ab = ((a * a * a) + (3 * a * a * b) + (3 * a * b * b) + (b * b * b));
+// document.write("<h1> answer of ab is :" + ab + "</h1>");
+
+// var vf = parseFloat( prompt("enter any number"));
+// var vi = parseFloat( prompt("enter any number"));
+
+// var vfvi =((vf * vf) - (vi * vi)) /(2 * a);
+// document.write("<h1> answer of initial velocity & final velocity is :" + vfvi + "</h1>");
+
+
+// var t = parseFloat( prompt("enter any number"));
+
+// var vf2vi2 = (vi * t) + ((1 * a * t * t) /2 );
+// document.write("<h1> answer of initial velocity & final velocity is :" + vf2vi2 + "</h1>");
+
+var Name = prompt("What is your name");
+document.write("your name is" + Name)
+var english = parseFloat( prompt("ente your english marks"));
+var urdu = parseFloat( prompt("ente your urdu marks"));
+var physics = parseFloat( prompt("ente your physics marks"));
+var chemistry = parseFloat( prompt("ente your chemistry marks"));
+var computer = parseFloat( prompt("ente your computer marks"));
+
+var total = (english + urdu + physics + chemistry + computer);
+document.write("total marks is :" + total );
+
+var pc = ((total / 250) * 100);
+document.write("your percentage is :" + pc + "%");
+
+
+
+
+
+
+
+
+
